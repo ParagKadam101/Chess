ae5900887d5369f6f60e71ea44f0cb23615a415c
paragkadam101 <paragkadam101@gmail.com>
2024-12-20 14:46:31 +0530

    Added main class

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,9 +1,37 @@
-import com.technogise.chess.Board;
+import com.technogise.chess.*;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidPositionException {
         Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type 'exit' to quit");
+        String input;
+        while (true) {
+            System.out.print("\nInput - ");
+            input = scanner.nextLine();
+            if(input.equals("exit")) break;
+            Piece piece = getPiece(input);
+            board.setPiece(piece);
+            List<String> possibleMovements = piece.getPossibleMovements().stream().sorted().collect(Collectors.toList());
+            String movementsString = String.join(", ", possibleMovements);
+            System.out.println(movementsString);
+        }
+    }
 
+    private static Piece getPiece(String input) throws InvalidPositionException {
+        String[] pieceAndPosition = input.replaceAll("\\s", "").split(",");
+        String pieceName = pieceAndPosition[0];
+        String piecePosition = pieceAndPosition[1];
+        return switch (pieceName) {
+            case "Pawn"  -> new Pawn(piecePosition);
+            case "Queen" -> new Queen(piecePosition);
+            case "King"  -> new King(piecePosition);
+            default -> throw new IllegalArgumentException("Invalid piece entered: " + pieceName);
+        };
     }
 }
